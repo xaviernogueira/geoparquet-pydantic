7e4df3cb476ffe01daf3e562b90150f5c36b6c6d
Xavier Nogueira <58796351+xaviernogueira@users.noreply.github.com>
2024-01-30 18:10:27 -0500

    Draft version done

--- geoparquet_pydantic/__init__.py
+++ geoparquet_pydantic/__init__.py
@@ -1,4 +1,5 @@
 import shapely
+import json
 import pyarrow
 import pyarrow.parquet as parquet
 from geojson_pydantic.geometries import (
@@ -14,41 +15,58 @@ def to_wkb(self: _GeometryBase) -> bytes:
     """Converts the GeoJSON object to WKB format."""
     return shapely.wkb.dumps(self.model_dump())
 
+
 _GeometryBase.wkb = property(to_wkb)
 
 
 def _get_geom_types(features: list[Feature]) -> list[str]:
     return list(set([feature.geometry.type for feature in features]))
 
+
+def _update_metadata(table: pyarrow.Table, metadata: dict) -> pyarrow.Table:
+    new_metadata = table.schema.metadata.copy()
+    for k, v in metadata.items():
+        new_metadata[k] = json.dumps(v).encode("utf-8")
+    return table.replace_schema_metadata(new_metadata)
+
+
 def to_geoparquet(
     self: FeatureCollection,
-    version: str = '1.0',
+    path: str,
+    version: str = "1.0",
     **kwargs,
-) -> None:
-    """Writes the FeatureCollection to a GeoParquet file."""
+) -> str:
+    """Writes the FeatureCollection to a GeoParquet file.
+
+    Args:
+        path (str): The path to the output file.
+        version (str, optional): The GeoParquet version. Defaults to '1.0'.
+    Returns:
+        The path of the saved GeoParquet.
+
+    """
 
     # write table
     table = pyarrow.Table.from_pydict(
-       {
-            'geometry': [feature.geometry.wkb for feature in self.features],
-       },
+        {
+            "geometry": [feature.geometry.wkb for feature in self.features],
+        },
     )
-    parquet.write_table()
 
     # update metadata
     metadata = {
-        'version': '1.0',
-        'primary_column': 'geometry',
-        'columns': {
-            'geometry': {
-                'encoding': 'WKB',
-                'geometry_types': _get_geom_types(self.features),
+        "version": "1.0",
+        "primary_column": "geometry",
+        "columns": {
+            "geometry": {
+                "encoding": "WKB",
+                "geometry_types": _get_geom_types(self.features),
             },
         },
     }
+    table: pyarrow.Table = _update_metadata(table, metadata)
+    parquet.write_table(table, path, **kwargs)
+    return path
 
 
 FeatureCollection.to_geoparquet = to_geoparquet
-
-
-

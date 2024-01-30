4df9620ea1553b1f99dfa79e4a820f8b37ceef8a
Xavier Nogueira <58796351+xaviernogueira@users.noreply.github.com>
2024-01-30 18:02:43 -0500

    Building out conversion

--- geoparquet_pydantic/__init__.py
+++ geoparquet_pydantic/__init__.py
@@ -1,9 +1,11 @@
 import shapely
+import pyarrow
 import pyarrow.parquet as parquet
 from geojson_pydantic.geometries import (
     _GeometryBase,
 )
 from geojson_pydantic.features import (
+    Feature,
     FeatureCollection,
 )
 
@@ -15,8 +17,36 @@ def to_wkb(self: _GeometryBase) -> bytes:
 _GeometryBase.wkb = property(to_wkb)
 
 
-def to_geoparquet(self: FeatureCollection) -> None:
-    ...
+def _get_geom_types(features: list[Feature]) -> list[str]:
+    return list(set([feature.geometry.type for feature in features]))
+
+def to_geoparquet(
+    self: FeatureCollection,
+    version: str = '1.0',
+    **kwargs,
+) -> None:
+    """Writes the FeatureCollection to a GeoParquet file."""
+
+    # write table
+    table = pyarrow.Table.from_pydict(
+       {
+            'geometry': [feature.geometry.wkb for feature in self.features],
+       },
+    )
+    parquet.write_table()
+
+    # update metadata
+    metadata = {
+        'version': '1.0',
+        'primary_column': 'geometry',
+        'columns': {
+            'geometry': {
+                'encoding': 'WKB',
+                'geometry_types': _get_geom_types(self.features),
+            },
+        },
+    }
+
 
 FeatureCollection.to_geoparquet = to_geoparquet
 

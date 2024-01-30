ec822bac6f9be4303d37c8f8b02df9a8d946c9f9
Xavier Nogueira <58796351+xaviernogueira@users.noreply.github.com>
2024-01-30 17:37:23 -0500

    Update __init__.py

--- geoparquet_pydantic/__init__.py
+++ geoparquet_pydantic/__init__.py
@@ -1,14 +1,24 @@
-import geojson_pydantic
 import shapely
-import multiprocessing
+import pyarrow.parquet as parquet
 from geojson_pydantic.geometries import (
     _GeometryBase,
 )
+from geojson_pydantic.features import (
+    FeatureCollection,
+)
 
 
-def to_wkb(self: _GeometryBase):
+def to_wkb(self: _GeometryBase) -> bytes:
     """Converts the GeoJSON object to WKB format."""
     return shapely.wkb.dumps(self.model_dump())
 
 _GeometryBase.wkb = property(to_wkb)
+
+
+def to_geoparquet(self: FeatureCollection) -> None:
+    ...
+
+FeatureCollection.to_geoparquet = to_geoparquet
+
+
 
